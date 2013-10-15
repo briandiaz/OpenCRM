35984f6cace3abef75c43672b61b1cb646e5f0a2
Brian Diaz <brian@briandiaz.me>
2013-10-14 23:19:46 -0400

    VS Sln Pushed

---
+++ OpenCRM/OpenCRM/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro;
+
+
+namespace OpenCRM
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSettings_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnLoginShow_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                frmSource.Navigate(new Uri("/Views/Login/LoginRegister.xaml", UriKind.Relative));
+                ShowFrame(true);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnHome_Click(object sender, RoutedEventArgs e)
+        {
+            ShowFrame(false);
+        }
+        private void ShowFrame(Boolean show)
+        {
+            if (show)
+            {
+                frmSource.Visibility = System.Windows.Visibility.Visible;
+                gridSplash.Visibility = System.Windows.Visibility.Hidden;
+            }
+            else
+            {
+                frmSource.Visibility = System.Windows.Visibility.Hidden;
+                gridSplash.Visibility = System.Windows.Visibility.Visible;
+            }
+        }
+    }
+}
---
+++ OpenCRM/OpenCRM/Models/Login/Login.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenCRM
+{
+    class LoginModel
+    {
+        public String Username { get; set; }
+        public String Password { get; set; }
+        public LoginModel(String username, String password)
+        {
+            this.Username = username;
+            this.Password = password;
+        }
+        public bool ValidateFields()
+        {
+            if (this.Username == null || this.Password == null)
+            {
+                throw new ArgumentNullException("You must instance this object");
+            }
+            if (this.Username.Equals("") && this.Password.Equals(""))
+            {
+                ShowMessage("You must enter your username and password");
+                return false;
+            }
+            else if (this.Password.Equals(""))
+            {
+                ShowMessage("You must enter your password.");
+                return false;
+            }
+            else if (this.Username.Equals(""))
+            {
+                ShowMessage("You must enter your username.");
+                return false;
+            }
+            else
+            {
+                ShowMessage("Correct!\n" + this.Username + "\n" + this.Password);
+                return true;
+            }
+        }
+        public void ShowMessage(String message)
+        {
+            System.Windows.MessageBox.Show(message);
+        }
+    }
+}
